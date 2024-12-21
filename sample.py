00e32e593a77490d6fd4c47176db2a666fb10fda
Afonso Taveira <astaa@iscte-iul.pt>
2024-12-21 13:44:01 +0000

    bloqueia chamadas de domínios externos

--- sample.py
+++ sample.py
@@ -31,19 +31,23 @@ class kamailio:
     # SIP request routing
     # -- equivalent of request_route{}
     def ksr_request_route(self, msg):
-        dominio = KSR.pv.get("$ru")
+        endereco = KSR.pv.get("$ru")
         KSR.info("===== request - from kamailio python script\n")
         KSR.info("===== method [%s] r-uri [%s]\n" % (KSR.pv.get("$rm"),KSR.pv.get("$ru")))
-        if KSR.is_REGISTER() and dominio == "sip:acme.pt":
+        if KSR.is_REGISTER() and endereco == "sip:acme.pt":
             KSR.registrar.save("location", 0)
             return 1
         else:
-            KSR.info("===== Comparação Falhou: Dominio [%s], Esperado [sip:acme.pt] =====\n" % dominio)
+            KSR.info("===== Comparação Falhou: Endereco [%s], Esperado [sip:acme.pt] =====\n" % endereco)
         
         if KSR.registrar.lookup("location") == 1:
-            KSR.info("=== Destination found. Fowarding the call =====\n")
+            dominio = KSR.pv.get("$fd")
+            if dominio != "sip:acme.pt":
+                KSR.info("===== Blocked because user belongs to external domain [%s] =====\n" % dominio)
+                return 1
+            KSR.info("=== Destination found. Fowarding the call [%s] =====\n" % dominio)
             KSR.tm.t_relay()
-            return 1
+            return 1  
         else:
             KSR.info("===== Destination not found in location table =====\n")
             KSR.sl.send_reply(404, "User Not Found")
